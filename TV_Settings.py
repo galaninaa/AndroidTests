6476d3c7b680336b9411e435eb40d7117385c672
galaninaa <tutoshka@mail.ru>
2017-07-19 01:54:03 -0700

    Added TV_Settings file

---
+++ TV_Settings.py
@@ -0,0 +1,31 @@
+Header = {'id':'com.talkatone.android:id/toolbar_title', 'xpath':'//android.widgetTextView[@resource-id="com.talkatone.android:id/toolbar_title"]'}
+
+AccountData = {'id':'com.talkatone.android:id/settingsAccountView', 'xpath':'//android.widget.RelativeLayout[@resource-id="com.talkatone.android:id/settingsAccountView"]'}
+
+AccountName = {'id':'com.talkatone.android:id/itemAccountNameTextView', 'xpath':'//android.widget.TextView[@resource-id="com.talkatone.android:id/itemAccountNameTextView"]'}
+
+AccountPhone = {'id':'com.talkatone.android:id/itemAccountPhoneTextView', 'xpath':'//android.widget.TextView[@resource-id="com.talkatone.android:id/itemAccountPhoneTextView"]'}
+
+AccountEmail = {'id':'com.talkatone.android:id/itemAccountEmailTextView', 'xpath':'//android.widget.TextView[@resource-id="com.talkatone.android:id/itemAccountEmailTextView"]'}
+
+BurnNumberButton = {'id':'com.talkatone.android:id/burn_number', 'xpath':'//android.widget.ImageView[@resource-id="com.talkatone.android:id/burn_number"]'}
+
+ShareNumberButton = {'id':'com.talkatone.android:id/share_number', 'xpath':'//android.widget.ImageView[@resource-id="com.talkatone.android:id/share_number"]'}
+
+SettingsScrollViewContainer = {'id': 'com.talkatone.android:id/settingsScrollContainer', 'xpath':'//android.widget.TextView[@resource-id="com.talkatone.android:id/settingsScrollContainer"]'}
+
+MenuItems = ['Get a New Number','Credits','Remove Ads', 'International Calls', 'Notifications & Sounds', 'Voicemail Greeting', 'Texting', 'Passcode',	'Blocked numbers', 'Miscellaneous', 'Quit Talkatone']
+
+SmallMenuItems = ['For international calls', 'Active','	Unlimited calls to select countries']
+
+PaidCredits = {'id':'com.talkatone.android:id/paid_credits_text', 'xpath':'//android.widget.TextView[@resource-id="com.talkatone.android:id/paid_credits_text"]'}
+#Only if Paid Credits text = 0.00
+BuyCredits = {'id':'com.talkatone.android:id/buy_credits_text', 'xpath':'//android.widget.TextView[@resource-id="com.talkatone.android:id/buy_credits_text"]'}
+
+RemoveAdsSwitch = {'id':'com.talkatone.android:id/switchWidget', 'xpath':'//android.widget.TextView[@resource-id="com.talkatone.android:id/switchWidget"]'}
+
+IternationalCallsSubState = {'id':'com.talkatone.android:id/subs_state', 'xpath':'//android.widget.TextView[@resource-id="com.talkatone.android:id/subs_state"]'}
+
+IternationalCallsSubStateMore = {'id':'com.talkatone.android:id/more', 'xpath':'//android.widget.TextView[@resource-id="com.talkatone.android:id/more"]'}
+
+ContactUs = {'id':'', 'xpath':'//android.widget.TextView[@text="Need help with Talkatone? Contact us"]'}
